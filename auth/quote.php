54d7ec5f26c56002437db40cbc7f4c33af825312
sagar <sagar@gmail.com>
2024-05-04 19:59:32 +0545

    quote box and style

--- auth/quote.php
+++ auth/quote.php
@@ -6,13 +6,80 @@
     <title>QUOTE SHARING WEBSITE</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+
+<style>
+
+.quote{
+    background-color: transparent;
+    
+    border: 3px solid black;
+  
+    height: auto;
+    width: 50%;
+    border-radius: 20px;
+    position: absolute;
+    left: 25%;
+    top: 140px;
+    color: white;
+}
+
+.qtxt{
+    font-size: large;
+    text-align: left;
+    padding: 30px;
+    z-index: 1;
+}
+.details{
+    display: flex;
+    gap: 40%;
+    padding: 30px;
+    z-index: 1;
+
+}
+
+</style>
+
+
+
 </head>
 <body>
    <header>
    
 <?php
 include_once 'header.php';
+
 ?>
+
+
+
+ <!-- quote box -->
+
+ <center>
+
+
+
+<div class="quote">
+
+    <p class="qtxt">
+       " hello "
+
+        <br>
+
+       <div class="details">
+
+        <strong>date: 9090</strong>
+        <strong> <i>-hello</i></strong>
+
+       </div>
+
+
+    </p>
+
+</div>
+
+
+ </center>
 
 
    </header>
